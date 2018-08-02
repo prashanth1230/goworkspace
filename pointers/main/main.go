2e2e8dcb183f1b14fde53df5b8dd01e099026d64
Prashanth Udupa <prashanth1230@gmail.com>
2018-08-02 11:47:54 +0530

    Referencing pointers

--- pointers/main/main.go
+++ pointers/main/main.go
@@ -7,4 +7,9 @@ func main() {
 	fmt.Println("Value of a and memory location of a are: ", x, &x)
 	fmt.Scan(&x)
 	fmt.Println("Updated Value of a and memory location of a are: ", x, &x)
+
+	a := 300
+	fmt.Println("Value and address: ", a, &a)
+	var b *int = &a
+	fmt.Println("Address, value and the pointer value of b: ", b, &b, *b)
 }
