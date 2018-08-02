eb1a09f68a796af55ee8208876edee19431302c4
Prashanth Udupa <prashanth1230@gmail.com>
2018-08-02 11:42:24 +0530

    Use scan to update the memory location.

--- pointers/main/main.go
+++ pointers/main/main.go
@@ -5,4 +5,6 @@ import "fmt"
 func main() {
 	x := 20
 	fmt.Println("Value of a and memory location of a are: ", x, &x)
+	fmt.Scan(&x)
+	fmt.Println("Updated Value of a and memory location of a are: ", x, &x)
 }
