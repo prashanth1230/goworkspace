1d57c43ca88445124d8d7539b328101ea97e8713
Prashanth Udupa <prashanth1230@gmail.com>
2018-08-05 19:42:03 +0530

    Added switch statement

--- controlFlow/main/main.go
+++ controlFlow/main/main.go
@@ -6,4 +6,15 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+
+	name := "Prashanth"
+
+	switch name {
+	case "Prashanth":
+		fmt.Println("Hello Prashanth")
+	case "Shruthi":
+		fmt.Println("Hello Shruthi")
+	default:
+		fmt.Println("Hello")
+	}
 }
