2072666b46fe69332af122f0bfa20e29b8c05ca9
Prashanth Udupa <prashanth1230@gmail.com>
2018-08-06 13:02:50 +0530

    Variand arguments for functions.

--- functions/main/functions.go
+++ functions/main/functions.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println(greet1("Prashanth"))
 	fmt.Println(greet2("Shruthi", "Shashank"))
+	fmt.Println(average(11, 22.0, 24, 45, 21.3))
 }
 
 // Named return
@@ -16,4 +17,13 @@ func greet1(name string) (hello string) {
 // Multiple return
 func greet2(name1 string, name2 string) (string, string) {
 	return fmt.Sprint("Hello ", name1), fmt.Sprint("Hello ", name2)
+}
+
+// Variadic inputs
+func average(sf ...float64) float64 {
+	total := 0.0
+	for _, v := range sf {
+		total += v
+	}
+	return total / float64(len(sf))
 }
